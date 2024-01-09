b7cab8026c44fb9c050e3c75404da663241ffa83
Boruch Khazanovich <boruk770@gmail.com>
2024-01-09 09:59:30 -0500

    small change

---
+++ nextjs-blog/pages/_app.js
@@ -0,0 +1,9 @@
+// To load global CSS to your application, create a file called pages/_app.js with the following content:
+// The default export of _app.js is a top-level React component that wraps all the pages in your application. 
+// You can use this component to keep state when navigating between pages, or to add global styles as we're doing here. Learn more about _app.js file.
+
+import 'C:/Users/Boruch/github/Portfolio/nextjs-blog/styles/global.css';
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+}
